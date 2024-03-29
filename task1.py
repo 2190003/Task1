bf0c896ed4be288aee7782e6ff457809e8fa5006
2190003 <kmi5044@naver.com>
2024-03-29 15:35:21 +0900

    수정

--- task1.py
+++ task1.py
@@ -1,7 +1,8 @@
+#2190003_강민아
 def a_plus_b(a, b, max_X):
     result = a * a * max_X + b
     return result
-#2190003_강민아
+
 x = 2
 y = 1
 h = 10
@@ -12,3 +13,5 @@ print(f'MAx: {h}')
 for max_X in range(11):  # max_X를 0부터 10까지 반복
     z = a_plus_b(x, y, max_X)
     print(f'{x}*{x}*{max_X} + {y} = {z}')
+
+
